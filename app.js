d0e186823b1894473fe667a194b58bd8acba45cc
smessina <info@silviomessina.com>
2020-10-20 21:40:42 -0300

    Revert "Server inicial"
    This reverts commit ea003918a732b7c2916d1e4f0be1eab7275c6809.

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const http = require('http')
-
-const port = 1000
-
-const server = (req, res) => {
-    res.end("I am you Server... jsssss")
-}
-
-http.createServer( server ).listen( port )
-
-
-
-
-
-
+console.log("I am you Script... jsssss")
