157efab6d66b0a88cea7ddf08f74ba4c4e0b0212
smessina <info@silviomessina.com>
2020-10-20 21:45:45 -0300

    Server inicial

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("I am you Script... jsssss")
+const http = require("http")
+
+const port = 1000   //mas allá del 1000 usualmente están disponibles
+
+const server = (req, res) => {  //parametros para configurar el servidor
+    res.end("<h1>Script!!!</h1>")
+}
+
+http.createServer( server ).listen( port ) //configuro las propiedades del servidor
