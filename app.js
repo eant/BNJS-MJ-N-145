baa4c16e0c5f420062c1327611b716194f12c070
smessina <info@silviomessina.com>
2020-10-20 22:10:24 -0300

    Sirvendo archivos estaticos con FileSystem

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require("http")
+const fs = require('fs')
 
 const port = 1000   //mas allá del 1000 usualmente están disponibles
 
 const server = (req, res) => {  //parametros para configurar el servidor
-    res.end("<h1>Script!!!</h1>")
+
+    fs.readFile('front/index.html', (error, content) => {
+
+        if(error){
+            res.writeHead(404, { "Content-Type" : "text/plain" })
+            res.end(`Malió sal...`)
+        } else {
+            res.writeHead(200, { "Content-Type" : "text/html" })
+            res.end(content)
+        }
+
+    })
 }
 
 http.createServer( server ).listen( port ) //configuro las propiedades del servidor
