7a3bf4500c4ee56c073ec3e3c9ff56c47fa75084
smessina <info@silviomessina.com>
2020-10-22 21:28:17 -0300

    Creando Front de Formulario de Contacto

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express()
 const port = 1000   //mas allá del 1000 usualmente están disponibles
 
 app.listen(port)
+app.use( express.static('public') )
+
 /*
 // Plantilla modelo para "endpoints" de express() //
 app.TIPO_HTTP("/RUTA", (req, res) => {
 
 })
 */
-app.get("/contacto", (req, res) => {
-    res.end('Desde acá vamos a contactarnos...')
+app.post("/enviar", (req, res) => {
+    res.end('Desde acá vamos a hacer algo muy loco...')
 })
