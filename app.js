a3697dc0bc6762c698ec641f84c5afaf80a87610
smessina <info@silviomessina.com>
2020-10-22 22:09:30 -0300

    Enviador de e-mails con Nodemailer

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express")
+const nodemailer = require("nodemailer")
 const app = express()
 
 const port = 1000   //mas allá del 1000 usualmente están disponibles
+
+const miniOutlook = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'raul80@ethereal.email',
+        pass: 'C9qndzm6Xq3w4f4Hrb'
+    }
+})
 
 app.listen(port)
 app.use( express.static('public') )
@@ -15,6 +25,13 @@ app.TIPO_HTTP("/RUTA", (req, res) => {
 */
 app.post("/enviar", (req, res) => {
     const contacto = req.body
-    console.log(contacto)
-    res.end('Desde acá vamos a hacer algo muy loco...')
+    
+    miniOutlook.sendMail({
+        from : contacto.correo, // sender address
+        to : "silvio.messina@eant.tech", // list of receivers
+        subject : `Asunto #${contacto.asunto}`, // Subject line
+        html : `<blockquote>${contacto.mensaje}</blockquote>"`, // html body
+    })
+
+    res.end('Desde acá vamos a enviar un email de contacto :o')
 })
