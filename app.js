94df7d83738af317791c78b367b5b28e0b54b90e
smessina <info@silviomessina.com>
2020-10-27 21:25:53 -0300

    Incorporando dotenv para variables de entorno

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express()
 const port = 1000   //mas allá del 1000 usualmente están disponibles
 
 const miniOutlook = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    host: process.env.HOST_MAIL,
+    port: process.env.PUERTO_MAIL,
     auth: {
-        user: 'raul80@ethereal.email',
-        pass: 'C9qndzm6Xq3w4f4Hrb'
+        user: process.env.CASILLA_MAIL,
+        pass: process.env.CLAVE_MAIL
     }
 })
 
@@ -29,6 +29,7 @@ app.post("/enviar", (req, res) => {
     miniOutlook.sendMail({
         from : contacto.correo, // sender address
         to : "silvio.messina@eant.tech", // list of receivers
+        replyTo : contacto.correo,
         subject : `Asunto #${contacto.asunto}`, // Subject line
         html : `<blockquote>${contacto.mensaje}</blockquote>"`, // html body
     })
